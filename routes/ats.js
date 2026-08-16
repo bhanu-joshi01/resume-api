cabf99774294dfb581819e2195459feb36831d42
Bhanu joshi <joshibhanu2006@gmail.com>
2026-08-16 11:52:35 +0530

    Delete routes/ats.js

--- routes/ats.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { checkATS } = require("../controllers/atsController");
-
-router.post("/check", checkATS);
-
-module.exports = router;
