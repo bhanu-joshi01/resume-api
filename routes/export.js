1b2e211d2ecbeade5fe9d2ca811b58868d1a3af6
Bhanu joshi <joshibhanu2006@gmail.com>
2026-08-16 11:54:08 +0530

    Update and rename export.js to exportRoutes.js

--- routes/export.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { exportDocument } = require("../controllers/exportController");
-
-router.post("/", exportDocument);
-
-module.exports = router;
