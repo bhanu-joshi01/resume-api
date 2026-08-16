69eab6d5e4c5ca31575e7ebd4a544269d8fbdd83
Bhanu joshi <joshibhanu2006@gmail.com>
2026-08-16 11:33:54 +0530

    Delete controllers/exportController.js

--- controllers/exportController.js
+++
@@ -1,8 +0,0 @@
-function exportDocument(req, res) {
-    res.status(202).json({
-        message: "Export Accepted",
-        format: req.body.format || "pdf"
-    });
-}
-
-module.exports = { exportDocument };
