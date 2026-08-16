47f04b77fae713664cf787ac60b201d6ae0d1ea8
Bhanu joshi <joshibhanu2006@gmail.com>
2026-08-16 12:02:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const logger = require("./middleware/logger");
 const routes = require("./routes");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
+// Enable CORS for all requests
+app.use(cors());
+
+// Parse JSON request body
 app.use(express.json());
-app.use(logger);
 
-app.get("/", function (req, res) {
-    res.json({ message: "Resume API is Running" });
-});
-
+// All API routes start from /api
 app.use("/api", routes);
 
-const PORT = 3000;
-
-app.listen(PORT, function () {
-    console.log("Server is running on port " + PORT);
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port`, process.env.PORT);
 });
