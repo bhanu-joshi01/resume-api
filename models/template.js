5f00979cf36e1f667b2065cd0bf1258128d29ab0
Bhanu joshi <joshibhanu2006@gmail.com>
2026-08-16 11:48:50 +0530

    Create template.js

---
+++ models/template.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Template extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Template.hasMany(models.Document, {
+        foreignKey: "templateId",
+        onDelete: "SET NULL",
+      });
+    }
+  }
+  Template.init(
+    {
+      name: DataTypes.STRING,
+      config: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Template",
+    },
+  );
+  return Template;
+};
