fa4ad51db676fbf11aa53934d2b1a786fcc32ed9
Bhanu joshi <joshibhanu2006@gmail.com>
2026-08-16 11:57:53 +0530

    Delete routes/tailoring.js

--- routes/tailoring.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { tailorResume } = require("../controllers/tailoringController");
-
-router.post("/", tailorResume);
-
-module.exports = router;
