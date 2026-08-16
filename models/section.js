b356e8e4b703da55e98820ca1bca6a33e914de23
Bhanu joshi <joshibhanu2006@gmail.com>
2026-08-16 11:47:57 +0530

    Create section.js

---
+++ models/section.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Section extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Section.belongsTo(models.Document, { foreignKey: "documentId" });
+      Section.hasMany(models.Item, {
+        foreignKey: "sectionId",
+        onDelete: "CASCADE",
+      });
+    }
+  }
+  Section.init(
+    {
+      heading: DataTypes.STRING,
+      position: DataTypes.INTEGER,
+      documentId: DataTypes.INTEGER,
+
+    isSidebar: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    },
+    {
+      sequelize,
+      modelName: "Section",
+    },
+  );
+  return Section;
+};
